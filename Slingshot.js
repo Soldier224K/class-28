ef4f48b5859d3c7563f08433fe10564723ff213b
Soldier224K <76217705+Soldier224K@users.noreply.github.com>
2021-01-27 20:54:49 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,24 @@
+class Slingshot{
+    constructor(body1,point2){
+        var options={
+            bodyA:body1,
+            pointB:point2,
+            lenght:10,
+            stiffness:0.04
+        }
+        this.pointB=point2
+        this.slingshot=Matter.Constraint.create(options)
+        World.add(world,this.slingshot)
+    
+    }
+    fly(){
+        this.slingshot.bodyA=null
+    }
+    display(){
+        if(this.slingshot.bodyA){
+        var pointA=this.slingshot.bodyA.position;
+        var pointB=this.pointB
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+    }
+}
